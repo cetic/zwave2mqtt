a812d7f0d10736a1302c15365a75ddd724650529
Benjamin <bbenjamin.bernaud@gmail.com>
2020-03-02 18:14:40 +0100

    Create install.sh
    Allow to install specifique docker and docker-compose for RPI0w

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!bin/bash
+
+echo ">>>Install dependancies"
+
+sudo apt-get install apt-transport-https
+
+echo ">>>Create /etc/apt/sources.list.d/docker.list"
+
+sudo sh -c 'echo "deb [arch=armhf] https://download.docker.com/linux/raspbian $(lsb_release -c -s) stable" > /etc/apt/sources.list.d/docker.list'
+
+echo ">>>Get the apt key :"
+
+wget -O - https://download.docker.com/linux/raspbian/gpg | sudo apt-key add -
+
+sleep 3
+
+echo ">>>Install Docker & Docker-ycompose:"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce=18.06.1~ce~3-0~raspbian
+
+sudo apt-get install docker-compose -y
+
+ 
+
+echo ">>>Optionnal : allow some users to use Dls /e   ocker (for example, user pea) :"
+
+sudo usermod -G docker pi
